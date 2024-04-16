de89fd3509f9b367c045795620287e12ae206b8a
MC <mihail9512@yahoo.co.uk>
2024-04-16 21:35:29 +0200

    Vectors and stuff

--- Week1/Code/vecmat/entities.py
+++ Week1/Code/vecmat/entities.py
@@ -37,21 +37,37 @@ class Vector:
         The two vectors must be of the same type and have the same dimensions
         
         @Input Params:
-        target (Vector): Target vector
+            target (Vector): Target vector
 
         @Output:
-        result (Vector): A new vector of the same type and dimensions"""
+            result (Vector): A new vector of the same type and dimensions"""
 
-        if self.type != target.type:
-            raise TypeError("Only vectors of the same type can be added together! Try transposing.")
-        if (self.dimCol != target.dimCol) or (self.dimRow != target.dimRow):
-            raise TypeError("Only vectors of the same dimensions can be added together!")
-
-        resultArr = []
-        for key, value in enumerate(self.values):
-            resultArr.append(value + target.values[key])
-        
+        self.checkSameType(target)
+        self.checkSameDimensions(target)
+        	
+        resultArr = [value + target.values[key] for key, value in enumerate(self.values)]        
         return Vector(resultArr, self.type)
 
+    def dot(self, target:'Vector'):
+        """Calculates the dot product of all values from the current vector to the target vector values.
+        The two vectors must be of the same type and have the same dimensions
         
+        @Input Params:
+            target (Vector): Target vector
 
+        @Output:
+            result (int): The result scalar"""
+        
+        self.checkSameType(target)
+        self.checkSameDimensions(target)
+
+        result = sum(map(lambda x,y: x*y, self.values, target.value))
+        return result
+        
+    def checkSameType(self, target:'Vector'):
+        if self.type != target.type:
+            raise TypeError("Only vectors of the same type can be added together! Try transposing.")
+        
+    def checkSameDimensions(self, target:'Vector'):
+        if (self.dimCol != target.dimCol) or (self.dimRow != target.dimRow):
+            raise TypeError("Only vectors of the same dimensions can be added together!")
